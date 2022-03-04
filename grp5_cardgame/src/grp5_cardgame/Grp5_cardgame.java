2b9ec5e372c3a87d58b37b95514baca4819db083
leewogsheridan <leewog@sheridancollege.ca>
2022-03-04 16:00:45 -0500

    MidTerm

---
+++ grp5_cardgame/src/grp5_cardgame/Grp5_cardgame.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package grp5_cardgame;
+
+/**
+ *
+ * @author 727
+ */
+public class Grp5_cardgame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
