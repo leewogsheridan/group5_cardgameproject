31376964001880e6c821aefaa137740d737333b2
leewogsheridan <leewog@sheridancollege.ca>
2022-04-20 18:20:17 -0400

    Added new specific classes for Blackjack game

--- grp5_cardgame/src/grp5_cardgame/Blackjack.java
+++ grp5_cardgame/src/grp5_cardgame/Blackjack.java
@@ -12,7 +12,7 @@ package grp5_cardgame;
  * @author 727
  * @author Gregory Lee Wo 2022
  */
-public class Blackjack {
+public class Blackjack extends Game {
 
     /**
      * @param args the command line arguments
@@ -20,6 +20,16 @@ public class Blackjack {
     public static void main(String[] args) {
         // TODO code application logic here
         //vicente angeles
+    }
+
+    @Override
+    public void play() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void declareWinner() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
     
 }
