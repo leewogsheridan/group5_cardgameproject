94cc6ba535dbd699f86aee35e46bb2fd2dc829b8
Kevin Wang <wang182@sheridancollege.ca>
2022-04-22 20:11:30 -0400

    wow

--- grp5_cardgame/src/grp5_cardgame/PlayingCard.java
+++ grp5_cardgame/src/grp5_cardgame/PlayingCard.java
@@ -37,11 +37,11 @@ public class PlayingCard extends Card {
 }
 
 enum Value {
-    ACE(11), TWO(2), THREE(3), FOUR(4), FIVE(5), SIX(6), SEVEN(7), EIGHT(8), NINE(9), TEN(10), JACK(11), QUEEN(12), KING(13);
+    ACE(11), TWO(2), THREE(3), FOUR(4), FIVE(5), SIX(6), SEVEN(7), EIGHT(8), NINE(9), TEN(10), JACK(10), QUEEN(10), KING(10);
 
     private final int value;
 
-    Value(int value) {
+    private Value(int value) {
         this.value = value;
     }
 
